65651ccbaaab1178fd9d5fd39d2cc3b402a1befc
Ademir Rivera <themrademir@gmail.com>
2025-01-13 14:10:37 -0600

    feat: ajustes en config de tailwind y creacion de archivos de prueba

---
+++ pages/products/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <h1 class="text-3xl font-bold underline">Products</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa perspiciatis
+        totam inventore earum ipsam ullam blanditiis, quidem assumenda vero illum
+        veniam similique ex quasi tenetur magni. At id tenetur nesciunt.
+      </p>
+    </div>
+  </template>
+  
+  <script setup></script>
+  
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    './app.vue',              // Archivo raíz de Nuxt 3
+    './components/**/*.{vue,js,ts}', // Componentes
+    './layouts/**/*.vue',     // Layouts
+    './pages/**/*.vue',       // Páginas
+    './plugins/**/*.{js,ts}', // Plugins
+    './nuxt.config.{js,ts}',  // Configuración de Nuxt
+  ],
   theme: {
     extend: {},
   },
