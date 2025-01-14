a8d5468f44038e0f3df9d491209e3aa29b314ecc
Ademir Rivera <themrademir@gmail.com>
2025-01-14 14:11:04 -0600

    feat: eliminando layout products e integrando detalle de producto

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -2,16 +2,22 @@
   <div>
     <div class="grid grid-cols-4 gap-5">
       <div v-for="p in products" :key="p.id">
-{{ p }}
+        <NuxtLink :to="`/products/${p.id}`">
+          <div class="bg-white p-5 rounded-lg shadow-lg">
+            <img
+              :src="p.image"
+              alt="product"
+              class="w-full h-48 object-cover"
+            />
+            <h2 class="text-lg font-semibold mt-2">{{ p.title }}</h2>
+            <p class="text-gray-500 mt-1">{{ p.price }}$</p>
+          </div>
+        </NuxtLink>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-definePageMeta({
-  layout: 'products'
-})
-
 const { data: products } = await useFetch('https://fakestoreapi.com/products')
 </script>
