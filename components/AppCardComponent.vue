f1341a9e6e26d2e1813b119da541c324068bbc9f
Ademir Rivera <themrademir@gmail.com>
2025-01-20 15:52:29 -0600

    feat: ajustes en pagina de productos

--- components/AppCardComponent.vue
+++ components/AppCardComponent.vue
@@ -1,13 +1,39 @@
 <template>
-    <div>
-
-    </div>
+  <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+      <NuxtLink :to="`/products/${product.id}`">
+        <div class="bg-white h-full p-5 rounded-lg shadow-lg">
+        <img
+          :src="product.image"
+          alt="product"
+          class="w-full h-48 object-cover"
+        />
+        <div class="grid grid-cols-3 gap-4">
+          <div class="col-span-2">
+            <h2 class="text-lg font-semibold mt-2">{{ product.title }}</h2>
+          </div>
+          <div class="flex flex-col items-end">
+            <p class="text-black mt-2">${{ product.price }}</p>
+          </div>
+        </div>
+        <p class="text-gray-500 mt-1 mb-6">{{ product.category }}</p>
+        <button
+          type="button"
+          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+        >
+          <span class="mr-1"> Agregar al carrito </span>
+        </button>
+      </div>
+    </NuxtLink>
+  </div>
 </template>
 
 <script setup>
-
+defineProps({
+  product: {
+    type: Object,
+    required: true
+  }
+})
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -6,27 +6,13 @@
       </span>
     </div>
 
-    <div class="grid grid-cols-4 gap-5">
-      <div v-for="p in products" :key="p.id">
-        <NuxtLink :to="`/products/${p.id}`">
-          <div class="bg-white p-5 rounded-lg shadow-lg">
-            <img
-              :src="p.image"
-              alt="product"
-              class="w-full h-48 object-cover"
-            />
-            <div class="grid grid-cols-3 gap-4">
-              <div class="col-span-2">
-                <h2 class="text-lg font-semibold mt-2">{{ p.title }}</h2>
-              </div>
-              <div class="flex flex-col items-end">
-                <p class="text-black mt-2">${{ p.price }}</p>
-              </div>
-            </div>
-            <p class="text-gray-500 mt-1">{{ p.category }}</p>
-          </div>
-        </NuxtLink>
-      </div>
+    <div class="grid grid-cols-4 gap-2 place-items-stretch mx-12">
+      <AppCardComponent
+        v-for="p in products"
+        :key="p.id"
+        :product="p"
+        class="mb-6"
+      />
     </div>
   </div>
 </template>
